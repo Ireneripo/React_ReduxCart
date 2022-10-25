32c35364a3defd87ab4ab863d0710d86ccd8deb4
Irene Riportella <riportellairene@gmail.com>
2022-10-25 16:01:05 +0200

    added backend with Firebase

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -6,6 +7,11 @@ import Products from './components/Shop/Products';
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-http-5ec53-default-rtdb.europe-west1.firebasedatabase.app/cart.json', { method: 'PUT', body: JSON.stringify(cart) });
+  }, [cart]);
 
   return (
     <Layout>
